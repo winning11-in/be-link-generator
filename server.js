fccb90d417763219fe6010a900aceaf30cf35df2
Ajay <ajay.goswami@coderower.com>
2025-12-17 14:53:46 +0530

    feat: update CORS configuration to allow additional origins for enhanced accessibility

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://fe-link-generator.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://fe-link-generator.vercel.app','http://localhost:8080','https://qr-craft-studio.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
