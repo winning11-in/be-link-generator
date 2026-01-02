9dec9d6e239c8f4a51cff4493242a17284ecc3c2
Ajay <ajay.goswami@coderower.com>
2026-01-02 22:11:24 +0530

    feat: remove logging functionality and related routes from the application

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { getAllUsersData, blockUser, deleteUser } from '../controllers/adminController.js';
-import { getLogs, cleanupLogs } from '../controllers/logController.js';
 import adminMiddleware from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
@@ -8,9 +7,5 @@ const router = express.Router();
 router.get('/users', adminMiddleware, getAllUsersData);
 router.put('/users/:id', adminMiddleware, blockUser);
 router.delete('/users/:id', adminMiddleware, deleteUser);
-
-// Logs routes
-router.get('/logs', adminMiddleware, getLogs);
-router.delete('/logs/cleanup', adminMiddleware, cleanupLogs);
 
 export default router;
